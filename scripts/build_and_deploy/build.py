c31833c4242ba61eff926c5b93b80f80dbd328ae
kairkhan24 <999.9plus0.1@gmail.com>
2023-07-11 09:18:31 +0600

    n10

--- scripts/build_and_deploy/build.py
+++ scripts/build_and_deploy/build.py
@@ -45,7 +45,7 @@ class Git:
 
     def move_to_commit(self, commit):
         os.chdir(DJANGO_SERVICE_PATH)
-        Log.success(f'after {os.getcwd()}')
+        Log.success(subprocess.getoutput('git log -n1 --format="%h"'))
         os.system(f'git checkout {commit}')
 
     def move_to_release_commit(self):
@@ -110,7 +110,7 @@ def build_and_push():
         git.move_to_release_commit()
         build_and_push_image(VISION_SERVICE_PATH, VISION_SERVICE_IMAGE_NAME, RELEASE_TAG)
 
-        Log.success(f'before {os.getcwd()}')
+        # Log.success(f'before {os.getcwd()}')
         git.move_to_commit(args.rollback_commit)
         build_and_push_image(VISION_SERVICE_PATH, VISION_SERVICE_IMAGE_NAME, ROLLBACK_TAG)
 
